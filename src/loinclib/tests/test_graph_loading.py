16d2b1d6c06bfa3b2ce7c7048ec5e99106d62d82
Shahim Essaid <shahim@essaid.com>
2024-08-14 15:09:43 -0700

    WIP

---
+++ src/loinclib/tests/test_graph_loading.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from unittest import TestCase
+
+LOINC_PATH = Path('loinc_release/2.67')
+TREE_PATH = Path('loinc_trees/2023-09-26')
+
+class TestLoading(TestCase):
+
+
+    def test_load_loinc_table(self):
+
+        pass
